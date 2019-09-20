c0e38d607929a9390f5a810dd5c505ce3e1385a2
Stefan Douma <douma4@gmail.com>
2019-09-20 21:23:29 +0200

    Implementation

---
+++ galtonboard.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"./galtonboard"
+)
+
+func main() {
+	galtonboard.Run()
+}
--- galtonboard/galtonboard.go
+++ galtonboard/galtonboard.go
@@ -1,20 +1,27 @@
 package galtonboard
 
+import (
+	"math"
+	"math/rand"
+	"fmt"
+	"time"
+)
+
 type Bullet struct {
-	position float32
+	position float64
 }
 
 func(b Bullet) DropLeft() Bullet {
-	var newPosition float32 = b.position - 0.5
+	var newPosition float64 = b.position - 0.5
 	return Bullet{position: newPosition}
 }
 
 func(b Bullet) DropRight() Bullet {
-	var newPosition float32 = b.position + 0.5
+	var newPosition float64 = b.position + 0.5
 	return Bullet{position: newPosition}
 }
 
-func(b Bullet) GetPosition() float32 {
+func(b Bullet) GetPosition() float64 {
 	return b.position;
 }
 
@@ -46,11 +53,10 @@ func TrayListFromlength(length int) []Tray {
 func(t Tray) WithBullet(bullet Bullet) Tray {
 	var list []Bullet;
 
-	for i := 0; i<len(list); i++ {
+	for i := 0; i<len(t.bullets); i++ {
 		list = append(list, t.bullets[i])
 	}
 	list = append(list, bullet)
-
 	return Tray{number: t.number, bullets: list}
 }
 
@@ -60,4 +66,84 @@ func(t Tray) GetNumber() int {
 
 func(t Tray) GetNumberOfBullets() int {
 	return len(t.bullets) 
+}
+
+type DropPolicyInterface interface {
+    direction() string
+}
+
+type LeftOrientedDropPolicy struct {}
+type RightOrientedDropPolicy struct {}
+type RandomDropPolicy struct {}
+
+func(p LeftOrientedDropPolicy) direction() string {
+	return "left"
+}
+
+func(p RightOrientedDropPolicy) direction() string {
+	return "right"
+}
+
+func(p RandomDropPolicy) direction() string {
+	rand.Seed(time.Now().UnixNano())
+	if(rand.Intn(2) == 1) {
+		return "left"
+	} else {
+		return "right"
+	}
+}
+
+type GaltonBoard struct {
+	bullets []Bullet
+	trays []Tray
+	dropPolicy DropPolicyInterface 
+}
+
+func(g GaltonBoard) DropBullets() {
+	for i := 0; i < len(g.bullets); i++ {
+		var bullet Bullet = g.bullets[i]
+		for x:= 0; x < g.getNumberOfDrops(); x++ {
+			if g.dropPolicy.direction() == "right" {
+				bullet = bullet.DropRight()
+			} else {
+				bullet = bullet.DropLeft()
+			}
+		}
+		g.updateResultingTray(bullet)
+	}
+}
+
+func(g GaltonBoard) getNumberOfDrops() int {
+	return len(g.trays) -1;
+}
+
+func(g GaltonBoard) getMiddle() float64 {
+	return float64(len(g.trays) / 2);
+}
+
+func(g GaltonBoard) updateResultingTray(bullet Bullet) {
+	var middle float64 = g.getMiddle()
+	var position float64 = middle + bullet.GetPosition()
+	var resultTray int = int(math.Floor(position));
+
+	for x := 0; x < len(g.trays); x++ {
+		if g.trays[x].GetNumber() == resultTray {
+			g.trays[x] = g.trays[x].WithBullet(bullet);		
+		}
+	}
+}
+
+func Run() {
+	bullets := BulletListFromlength(200)
+	trays := TrayListFromlength(10)
+	dropPolicy := RandomDropPolicy{}
+	board := GaltonBoard{bullets, trays, dropPolicy}
+	board.DropBullets()
+
+	for x:= 0; x < len(trays); x++ {	
+		for a := 0; a < trays[x].GetNumberOfBullets(); a++ {
+			fmt.Print("0")
+		}
+		fmt.Print("\n")
+	}
 }
